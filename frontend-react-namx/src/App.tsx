a1af732efab31b9d795a4dd7a962edffdd2d2ca1
fatimazahra ait lasri <fatimazahraaitlasri96@gmail.com>
2022-12-12 14:40:05 +0100

    create dashboard admin

--- frontend-react-namx/src/App.tsx
+++ frontend-react-namx/src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Dashboard from './pages/dashboard'
+import Cars from './components/cars';
+import Details from './components/detailles';
+import Orers from './components/orders';
+import Clients from './components/Clients';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+           <Route path="/admin" element={ <Dashboard/> }/>
+           <Route path="/dashboard/cars" element={ <Cars/> }/>
+           <Route path="/dasboard/detaills" element={ <Details/> }/>
+           <Route path="/dasboard/orders" element={ <Orers/> }/>
+           <Route path="/dasboard/Clients" element={ <Clients/> }/>
+        </Routes>
+    </Router>
+
+    
+    
   );
 }
 
 export default App;
+
+
+
---
+++ frontend-react-namx/src/pages/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DAshboard from '../components/buttonDachboard'
+
+
+function App() {
+  return (
+    <div className="App">
+     <DAshboard/>
+    </div>
+  );
+}
+
+export default App;
